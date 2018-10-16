1c4486d51e7c49f7b2385b8d2ed4cf7194d64357
Fernando Andreotti <fernando.andreotti@eng.ox.ac.uk>
2018-10-16 13:33:04 +0100

    Update run_spyder.sh

--- run_spyder.sh
+++ run_spyder.sh
@@ -1,26 +1,21 @@
 #!/bin/sh
 
-type=$1
 USER_NAME=`basename $HOME`
 
-sudo xhost + local:docker       # xhost+
-if [ $1 = "cpu" ]; then
-    echo "Mounting docker CPU image.."
-    docker run -ti --rm  \
-        -e DISPLAY=unix$DISPLAY \
-        -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-        -v `pwd`:/data \
-        andreotti/deeplearn:cpu
+sudo xhost + local:docker       # xhost+ slightly unsafe
 # assuming GPU is the standard
-else
-    echo "Mounting docker GPU image.."
-    nvidia-docker run -ti --rm \
+if [ -z "$1" ] || [ "$1" -ne "cpu" ]; then
+   echo "Mounting docker GPU image.."
+   nvidia-docker run -ti --rm \
      -e DISPLAY=$DISPLAY \
      -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
      -v `pwd`:/data \
      andreotti/deeplearn:gpu
+else 
+    echo "Mounting docker CPU image.."
+    docker run -ti --rm --device /dev/dri \     # dri
+        -e DISPLAY=unix$DISPLAY \               # outputing display
+        -v /tmp/.X11-unix:/tmp/.X11-unix:rw \   # also enable X11 socket
+        -v `pwd`:/data \                        # map current folder to /data
+        andreotti/deeplearn:cpu                 # image name    
 fi
-
-
-
-
