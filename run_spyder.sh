2ad0a0048b37194c2f6f5863b27c45fe182f299e
Fernando Andreotti <fernando.andreotti@eng.ox.ac.uk>
2018-10-16 15:15:29 +0100

    Changes to bash and gpu version

--- run_spyder.sh
+++ run_spyder.sh
@@ -1,26 +1,21 @@
 #!/bin/sh
 
-type=$1
 USER_NAME=`basename $HOME`
 
-sudo xhost + local:docker       # xhost+
-if [ $1 = "cpu" ]; then
+sudo xhost + local:docker       # xhost+ slightly unsafe
+# assuming GPU is the standard
+if [ -z "$1" ] || [ "$1" -ne "cpu" ]; then
+   echo "Mounting docker GPU image.."
+   nvidia-docker run -ti --rm \
+     -e DISPLAY=$DISPLAY \
+     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+     -v `pwd`:/data \
+     andreotti/deeplearn:gpu
+else 
     echo "Mounting docker CPU image.."
     docker run -ti --rm  \
         -e DISPLAY=unix$DISPLAY \
         -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
         -v `pwd`:/data \
         andreotti/deeplearn:cpu
-# assuming GPU is the standard
-else
-    echo "Mounting docker GPU image.."
-    nvidia-docker run -ti --rm \
-     -e DISPLAY=$DISPLAY \
-     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-     -v `pwd`:/data \
-     andreotti/deeplearn:gpu
 fi
-
-
-
-
