94d7552a362ae78aaec488f6496c6307c2816a35
Fernando Andreotti <fernando.andreotti@eng.ox.ac.uk>
2018-11-29 15:18:09 +0000

    Created yml files for python conda dependencies

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-type=$1
 USER_NAME=`basename $HOME`
 
 sudo xhost + local:docker       # xhost+ slightly unsafe
@@ -12,15 +11,11 @@ if [ -z "$1" ] || [ "$1" -ne "cpu" ]; then
      -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
      -v `pwd`:/data \
      andreotti/deeplearn:gpu
-else 
+else
     echo "Mounting docker CPU image.."
-    docker run -ti --rm --device /dev/dri \     # dri
-        -e DISPLAY=unix$DISPLAY \               # outputing display
-        -v /tmp/.X11-unix:/tmp/.X11-unix:rw \   # also enable X11 socket
-        -v `pwd`:/data \                        # map current folder to /data
-        andreotti/deeplearn:cpu                 # image name    
+    docker run -ti --rm  \
+        -e DISPLAY=unix$DISPLAY \
+        -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+        -v `pwd`:/data \
+        andreotti/deeplearn:cpu
 fi
-
-
-
-
